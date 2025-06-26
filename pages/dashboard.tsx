fc4f99bcf23264395d150679afcf5e77ab8999b7
Prajwal Ghadge <prajwalghadge2005@gmail.com>
2025-06-26 17:52:45 +0530

    v 1.4 name error solved

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -107,7 +107,7 @@ export default function Dashboard() {
       emoji: '🪟', 
       desc: '64-bit installer',
       filename: 'Aetheria ai Setup 1.0.4.exe',
-      path: 'https://github.com/GodBoii/AI-OS-website/releases/download/v1.0.0/Aetheria ai Setup 1.0.4.exe',
+      path: 'https://github.com/GodBoii/AI-OS-website/releases/download/v1.0.0/Aetheria.ai.Setup.1.0.4.exe',
       available: true
     },
     { 
